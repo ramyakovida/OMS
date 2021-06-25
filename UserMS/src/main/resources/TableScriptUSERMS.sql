2ed20c453531be7fb382b6fb061781369bdb50ec
ramya <ramya.kovida9@gmail.com>
2021-06-25 21:50:03 +0530

    updated tablescripts

--- UserMS/src/main/resources/TableScriptUSERMS.sql
+++ UserMS/src/main/resources/TableScriptUSERMS.sql
@@ -16,6 +16,12 @@ constraint buyer_pk primary key (buyer_id)
 
 );
 
+insert into buyer values('B1001','Jack','jack@gmail.com',9009009001,'Jack@123','true',1000,'true');
+insert into buyer values('B1002', 'Jill', 'jill@gmail.com',  9009009002,  'Jill@123', 'true', 1500, 'true');
+insert into buyer values('B1003','Sam','jack@gmail.com',9009009003,'Sam@123','true',2000,'true');
+insert into buyer values('B1004','Toby','toby@gmail.com',9009009004,'Toby@123','true',750,'true');
+insert into buyer values('B1005','Albert','albert@gmail.com',9009009005,'Albert@123','true',10000,'true');
+
 
 create table seller (
 seller_id varchar(30) not null,
@@ -29,6 +35,11 @@ constraint seller_pk primary key (seller_id)
 
 );
 
+
+insert into seller values('S2001','Zan','zan@gmail.com',9001900001,'Zan@123','true');
+insert into seller values('S2002','Ben','ben@gmail.com',9001900002,'Ben@123','true');
+insert into seller values('S2003','Mike','mike@gmail.com',9001900001,'Mike@123','true');
+
 create table wishlist (
 buyer_id varchar(30),
 prod_id varchar(30),
@@ -37,6 +48,14 @@ prod_id varchar(30),
 constraint wishlist_pk primary key (prod_id,buyer_id)
 
 );
+
+insert into wishlist values('B1001','P3003');
+insert into wishlist values('B1003','P3001');
+insert into wishlist values('B1001','P3002');
+insert into wishlist values('B1005','P3006');
+insert into wishlist values('B1004','P3004');
+insert into wishlist values('B1005','P3005');
+
 
 create table cart (
 buyer_id varchar(30),
@@ -47,3 +66,10 @@ quantity int,
 constraint wishlist_pk primary key (prod_id,buyer_id)
 
 );
+
+insert into cart values('B1001','P3003',1);
+insert into cart values('B1003','P3001',2);
+insert into cart values('B1001','P3002',1);
+insert into cart values('B1005','P3006',3);
+insert into cart values('B1004','P3004',1);
+insert into cart values('B1005','P3005',1);
